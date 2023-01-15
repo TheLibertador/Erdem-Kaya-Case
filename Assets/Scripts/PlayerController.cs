619dff6595218012063902025db8d357c222e144
Erdem Kaya <195040055@stu.istinye.edu.tr>
2023-01-15 16:30:33 +0300

    add PlayerController
    a simple player controller has been created with basic movement and jump function. A joystick package has been used for the UI purposes

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,61 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Assertions.Must;
+
+public class PlayerController : MonoBehaviour
+{
+    private Rigidbody m_Rigidbody;
+    private float speed = 10f;
+    private float jumpForce = 5f;
+    private bool isGrounded;
+    
+    [SerializeField] private DynamicJoystick joystick;
+    
+    
+
+    private void Start()
+    {
+        m_Rigidbody = GetComponent<Rigidbody>();
+    }
+    
+    private void FixedUpdate()
+    {
+       Move();
+
+       if (Input.GetKey(KeyCode.A))
+       {
+           Jump();
+       }
+    }
+
+    private void Move()
+    {
+        m_Rigidbody.velocity =
+            new Vector3(joystick.Horizontal * speed, m_Rigidbody.velocity.y, joystick.Vertical * speed);
+    }
+
+    public void Jump()
+    {
+        if (CheckIsGrounded())
+        {
+            m_Rigidbody.AddForce(new Vector3(0, jumpForce,0 ), ForceMode.Impulse);
+        }
+       
+    }
+
+    private bool CheckIsGrounded()
+    {
+        if (transform.position.y > 2.5f)
+        {
+            isGrounded = false;
+        }
+        else
+        {
+            isGrounded = true;
+        }
+
+        return isGrounded;
+    }
+}
